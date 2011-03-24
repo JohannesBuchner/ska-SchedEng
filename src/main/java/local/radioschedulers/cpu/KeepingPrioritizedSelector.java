b226d03a7a72d84fde4950113931085c71aeb2a9
Johannes Buchner <johannes.buchner.acad@gmx.com>
2011-03-24 15:46:24 +1300

    added strategy that is similar to PrioritizedSelector, but keeps tasks as long as it can

---
+++ src/main/java/local/radioschedulers/cpu/KeepingPrioritizedSelector.java
@@ -0,0 +1,52 @@
+package local.radioschedulers.cpu;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+
+import local.radioschedulers.Job;
+import local.radioschedulers.JobCombination;
+
+/**
+ * Jobs are continued if possible. If possible, additional jobs are selected
+ * proportional to their priority.
+ * 
+ * @author Johannes Buchner
+ */
+public class KeepingPrioritizedSelector extends PrioritizedSelector {
+
+	private static JobCombination lastJobCombination;
+
+	protected Comparator<JobCombination> generateComparator(
+			HashMap<Job, Double> timeleft) {
+		return new Comparator<JobCombination>() {
+
+			@Override
+			public int compare(JobCombination o1, JobCombination o2) {
+				// prefer the ones that were in the last
+				if (lastJobCombination != null) {
+					boolean continued1 = lastJobCombination.jobs.contains(o1);
+					boolean continued2 = lastJobCombination.jobs.contains(o2);
+					if (continued1 && !continued2)
+						return -1;
+					else if (!continued1 && continued2)
+						return 1;
+				}
+
+				// if that's the same, prefer higher priority
+				// this already incorporates that more (adding additional jobs)
+				// is better.
+				Double p1 = o1.calculatePriority();
+				Double p2 = o2.calculatePriority();
+				return p1.compareTo(p2);
+			}
+		};
+	}
+
+	@Override
+	public Collection<JobCombination> select(Collection<JobCombination> list) {
+		Collection<JobCombination> l = super.select(list);
+		lastJobCombination = l.iterator().next();
+		return l;
+	}
+}
