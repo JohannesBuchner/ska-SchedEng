b89000ba6d42e721c462f2d9e11a05c91927d253
Johannes Buchner <johannes.buchner.acad@gmx.com>
2011-04-15 07:39:04 +1200

    improved lastEntry calculation

--- src/main/java/local/radioschedulers/ScheduleSpace.java
+++ src/main/java/local/radioschedulers/ScheduleSpace.java
@@ -1,6 +1,5 @@
 package local.radioschedulers;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,11 +17,13 @@ import org.codehaus.jackson.annotate.JsonTypeInfo;
  * 
  * @author Johannes Buchner
  */
-@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
-public class ScheduleSpace implements Iterable<Entry<LSTTime, Set<JobCombination>>> {
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+public class ScheduleSpace implements
+		Iterable<Entry<LSTTime, Set<JobCombination>>> {
 	public static final int LST_SLOTS_MINUTES = 15;
 	public static final int LST_SLOTS_PER_DAY = 60 * 24 / LST_SLOTS_MINUTES;
 
+	private LSTTime last = new LSTTime(0, 0);
 	private Map<LSTTime, JobCombinationChoice> possibles = new TreeMap<LSTTime, JobCombinationChoice>();
 
 	/*
@@ -31,10 +32,12 @@ public class ScheduleSpace implements Iterable<Entry<LSTTime, Set<JobCombination
 	@SuppressWarnings("serial")
 	public static class JobCombinationChoice extends HashSet<JobCombination> {
 	}
-	
+
 	private void createIfNeeded(LSTTime t) {
 		if (!possibles.containsKey(t)) {
 			possibles.put(t, new JobCombinationChoice());
+			if (t.isAfter(last))
+				last = new LSTTime(t.day, t.minute);
 		}
 	}
 
@@ -60,13 +63,13 @@ public class ScheduleSpace implements Iterable<Entry<LSTTime, Set<JobCombination
 	}
 
 	public LSTTime findLastEntry() {
-		return Collections.max(possibles.keySet());
+		return last;
 	}
-	
-	public Map<LSTTime, JobCombinationChoice> getPossibles() {
+
+	Map<LSTTime, JobCombinationChoice> getPossibles() {
 		return possibles;
 	}
-	
+
 	@JsonIgnore
 	@Override
 	public Iterator<Entry<LSTTime, Set<JobCombination>>> iterator() {
